ced9792e1004b655b21bac216a57baaecad0176b
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-12 10:05:46 +0200

    commit

--- webscrapping.py
+++ webscrapping.py
@@ -111,8 +111,6 @@ class Planning():
                     liste_fin[2]+='T'
                     liste_debut[2]+='T'
                     for i in range(5):
-                        liste_fin[2]+='T'
-                        liste_debut[2]+='T'
                         str_debut+=liste_debut[i]
                         str_fin+=liste_fin[i]
                     #On reformule le dictionnaire avec les informations classées
