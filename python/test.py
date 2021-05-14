b152c0fb955d94b29bf060a6fa0822aa11f1f162
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-14 09:28:56 +0200

    commit

--- python/test.py
+++ python/test.py
@@ -1,5 +1,6 @@
 import commandes,webscraping
 
 user = webscraping.scraping().getting_identification_from_database()
-webscraping.scraping().get_planning(username=user['username'],password=user['password'])
+print(webscraping.scraping().get_planning(username=user['username'],password=user['password']))
+print(webscraping.scraping().get_marks(username=user['username'],password=user['password']))
 commandes.envoie_planning()
