b075b6e4490c17c77b823bdbfdabacbe87a02b2b
Brieuc DANDOY <brieuc.dandoy@isen.yncrea.fr>
2021-05-17 16:16:20 +0200

    avec changment

--- python/webscraping.py
+++ python/webscraping.py
@@ -118,12 +118,22 @@ class scraping():
 
 
                     #pour les fichiers csv
-                    date_debut= re.sub("[-]",'/', re.search("[0-9-]+[0-9]+",dict["start"]).group())
+                    date_buffer_debut =  re.search("[0-9-]+[0-9]+",dict["start"]).group()
+                    date_debut= re.sub("[-]",'/', date_buffer_debut)
+                    
                     heure_debut=re.search("[0-9:]{5}",dict["start"]).group()
-                    date_fin=re.sub("[-]",'/', re.search("[0-9-]+[0-9]+",dict["end"]).group())
+
+                    date_buffer_fin = re.search("[0-9-]+[0-9]+",dict["end"]).group()
+                    date_fin=re.sub("[-]",'/', date_buffer)
+
                     heure_fin=re.search("[0-9:]{5}",dict["end"]).group()
 
-                    
+                    #pour api google agenda
+                    heure_et_jour_debut = date_buffer_debut + 'T' + heure_debut + '+02:00'
+                    heure_et_jour_fin = date_buffer_fin + 'T' + heure_fin + '+02:00'
+
+
+
                     #On reformule le dictionnaire avec les informations classées
                     dictionnaire["salle"]=salle
                     dictionnaire["professeur"] = professeur
