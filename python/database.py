6c2d8871017ddb3d61e873d942f73d91a06949ac
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-18 08:55:07 +0200

    filling database with first user

--- python/database.py
+++ python/database.py
@@ -1,12 +1,21 @@
 import sqlite3
 
-connexion = sqlite3.connect("python/sqlite.db")
+connexion = sqlite3.connect("python/database.db")
 
 cursor = connexion.cursor()
 
 #Add user to user table :
-cursor.execute("""INSERT INTO user VALUES ("hugozz","mdp","hugo")""")
+username = 'p64059'
+password = 'ny5mJb8z'
+email = 'hugo.demenez@isen.yncrea.fr'
+
+
+parametres = (username,password,email)
+command = "INSERT OR REPLACE INTO user VALUES (?,?,?)"
+cursor.execute(command,parametres)
+connexion.commit()
+
+
 
 cursor.execute("SELECT * FROM user")
-
 print(cursor.fetchall())
