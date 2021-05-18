2186502706a5356939e5bd08bae18fbdcc1df87f
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-18 12:34:33 +0200

    commit

--- python/api-google-caldendar.py
+++ python/api-google-caldendar.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-from Google import Create_Service
 from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from googleapiclient.discovery import build
 
@@ -8,8 +6,6 @@ CLIENT_SECRET_FILE  = 'C:/Users/brieu/Desktop/code_secret_client_902657260294-uo
 API_NAME = 'calendar'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-
 
 flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
 cred = flow.run_local_server()
@@ -30,5 +26,5 @@ def ajouter_event(jour_et_heure_debut,jour_et_heure_fin,intitule,prof,salle,):
     }
     }
 
-    event = service.events().insert(calendarId='primary', body=evenement).execute()
+    service.events().insert(calendarId='primary', body=evenement).execute()
 
