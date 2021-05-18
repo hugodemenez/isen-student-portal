0efb252a4dfa4aeaa0a5df303c3067e2a4675abe
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-18 09:10:39 +0200

    changing database

--- python/database.py
+++ python/database.py
@@ -5,15 +5,22 @@ connexion = sqlite3.connect("python/database.db")
 cursor = connexion.cursor()
 
 #Add user to user table :
-username = 'p63058'
-password = 'vYYuQxIx'
-email = 'guillaume.gulli@isen.yncrea.fr'
+users=[{
+'username' : 'p63058',
+'password' : 'vYYuQxIx',
+'email' : 'guillaume.gulli@isen.yncrea.fr'
+},
+{
+'username' : 'p64059',
+'password' : 'ny5mJb8z',
+'email' : 'hugo.demenez@isen.yncrea.fr'
+}]
 
-
-parametres = (username,password,email)
-command = "INSERT OR REPLACE INTO user VALUES (?,?,?)"
-cursor.execute(command,parametres)
-connexion.commit()
+for user in users:
+    parametres = (user['username'],user['password'],user['email'])
+    command = "INSERT OR REPLACE INTO user VALUES (?,?,?)"
+    cursor.execute(command,parametres)
+    connexion.commit()
 
 
 
