aef2c3d8e57834c0cf4b6294eb92191c8fbd1695
Brieuc DANDOY <brieuc.dandoy@isen.yncrea.fr>
2021-05-19 14:00:56 +0200

    test

--- python/test.py
+++ python/test.py
@@ -3,3 +3,38 @@ import commandes,webscraping
 
 
 print(webscraping.scraping().get_planning( username = 'p64059',password = 'ny5mJb8z'))
+
+from pprint import pprint
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+
+CLIENT_SECRET_FILE  = 'C:/Users/brieu/Desktop/code_secret_client_902657260294-uosch8kaf16e1tjuq18vvkdvov46hj0d.apps.googleusercontent.com.json'
+API_NAME = 'calendar'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+cred = flow.run_local_server()
+service = build(API_NAME, API_VERSION, credentials=cred)
+
+
+
+
+event = {
+  'summary': 'repas',
+  'location': 'maison',
+  'description': 'A table',
+  'start': {
+    'dateTime': '2021-05-18T12:00:00+02:00',
+    'timeZone': 'Europe/Paris',
+  },
+  'end': {
+    'dateTime': '2021-05-18T13:30:00+02:00',
+    'timeZone': 'Europe/Paris',
+  }
+}
+
+event = service.events().insert(calendarId='primary', body=event).execute()
