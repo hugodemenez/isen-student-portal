e8c1334fee66e7e12d5d235709e66b85f8b4c113
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-19 16:03:28 +0200

    ok

--- python/api-google-caldendar.py
+++ python/api-google-caldendar.py
@@ -3,15 +3,18 @@ from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from googleapiclient.discovery import build
 
 class Calendrier():
+    def __init__(self):
+            self.CLIENT_SECRET_FILE  = r'C:\Users\Hugo\OneDrive\Github\Projet_2021_Informatique\json_files\code_secret_client_902657260294-uosch8kaf16e1tjuq18vvkdvov46hj0d.apps.googleusercontent.com.json'
+            self.API_NAME = 'calendar'
+            self.API_VERSION = 'v3'
+            self.SCOPES = ['https://www.googleapis.com/auth/calendar']
+            self.flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            self.cred = self.flow.run_local_server()
+            self.service = build(API_NAME, API_VERSION, credentials=self.cred)
+
     def ajouter_event(date_google_api_debut,date_google_api_fin,cours,professeur,salle):
         try:
-            CLIENT_SECRET_FILE  = r'C:\Users\Hugo\OneDrive\Github\Projet_2021_Informatique\json_files\code_secret_client_902657260294-uosch8kaf16e1tjuq18vvkdvov46hj0d.apps.googleusercontent.com.json'
-            API_NAME = 'calendar'
-            API_VERSION = 'v3'
-            SCOPES = ['https://www.googleapis.com/auth/calendar']
-            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-            cred = flow.run_local_server()
-            service = build(API_NAME, API_VERSION, credentials=cred)
+
             event = {
             'summary': cours,
             'location': salle,
