32fbb907848ad628fb8b7002fb2427eff2870bb6
Mohamed Amine BENNOUNA <mohamed_amine.bennouna@isen.yncrea.fr>
2021-05-19 15:33:52 +0200

    test

--- php/verification.php
+++ php/verification.php
@@ -1,36 +1,35 @@
 <?php
+ini_set('display_errors', 'on');
+ 
 session_start();
 if(isset($_POST['username']) && isset($_POST['password']))
 {
-    // connexion à la base de données
-    $db_username = 'root';
-    $db_password = 'mot_de_passe_bdd';
-    $db_name     = 'nom_bdd';
-    $db_host     = 'localhost';
-    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
-           or die('could not connect to database');
-    
+// Connexion
+try {
+   $dbname='database.db'; 
+   if(!class_exists('SQLite3'))  die("SQLite 3 NOT supported."); 
+   $base =  new SQLite3($dbname, 0666);
+   echo "SQLite 3 supported."; 
+} catch (SQLiteException $e) {
+    die("La création ou l'ouverture de la base [$base] a échouée ".
+         "pour la raison suivante: ".$e->getMessage());
+}
     // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
     // pour éliminer toute attaque de type injection SQL et XSS
-    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
-    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
-    
+    $username = $_POST['username']; 
+    $password = $_POST['password'];
     if($username !== "" && $password !== "")
-    {
-        $requete = "SELECT count(*) FROM utilisateur where 
-              nom_utilisateur = '".$username."' and mot_de_passe = '".$password."' ";
-        $exec_requete = mysqli_query($db,$requete);
-        $reponse      = mysqli_fetch_array($exec_requete);
-        $count = $reponse['count(*)'];
-        if($count!=0) // nom d'utilisateur et mot de passe correctes
-        {
-           $_SESSION['username'] = $username;
-           header('Location: principale.php');
-        }
-        else
-        {
-           header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
-        }
+    {$query = "SELECT COUNT(*) as count FROM `user` WHERE `username` = :username AND `password` = :password"; 
+      $results = $base->query($query);
+       $row = $results->fetchArray(); 
+       if(count($row)>0)
+        { 
+            header('location:principale.php'); }	
+		}else{
+			$_SESSION['error'] = "Invalid username or password";
+			header('location:login.php?erreur=1');
+		}
+        
     }
     else
     {
@@ -41,5 +40,5 @@ else
 {
    header('Location: login.php');
 }
-mysqli_close($db); // fermer la connexion
+$bd = null; // fermer la connexion
 ?>
