5047f0f0afe48d1e1bc7ae2726cb2321e1f76ce6
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-31 17:05:47 +0200

    commit

--- php/login.php
+++ php/login.php
@@ -11,11 +11,9 @@
             <form action="verification.php" method="POST">
                 <h1>Connexion</h1>
                 
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+                <input type="text" placeholder="username" name="username" required>
 
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+                <input type="password" placeholder="password" name="password" required>
 
                 <input type="submit" id='submit' value='LOGIN' >
                 <?php
