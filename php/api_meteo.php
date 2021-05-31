7cfc350b3ca7afbb4091a078f831b1718460387a
BrieucDandoy <brieuc.dandoy@gmail.com>
2021-05-31 15:00:30 +0200

    api meteo

--- php/api_meteo.php
+++ php/api_meteo.php
@@ -1,4 +1,25 @@
-<?php
-$test = api.openweathermap.org/data/2.5/weather?q={city name}&appid={ef7a9b79630b7257c06221b69e13fdb9}
-echo ($test)
+<?php 
+$apiKey = "ef7a9b79630b7257c06221b69e13fdb9";
+$cityId = "2998324";
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=fr&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$meteo = json_decode($response);
+/* echo $data->weather[0]->description . "<br />"; //exemple pour avoir des trucs
+echo $data->weather[0]->id	; pour plus d'infos:https://openweathermap.org/current#format
+
+IL FAUT INSTALLER LA BLIBLIOTHEQUE CURL
+
+ */ 
+
 ?>
