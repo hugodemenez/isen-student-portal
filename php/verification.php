d3312f10b9321aca512ab91f9aedd0b3845a0e40
Hugo Demenez <hdemenez@hotmail.fr>
2021-05-31 16:10:43 +0200

    connection form working

--- php/verification.php
+++ php/verification.php
@@ -1,8 +1,18 @@
 <?php
 ini_set('display_errors', 'on');
 session_start();
-if(isset($_POST['username']) && isset($_POST['password']))
-{
-   echo $_POST['username'];
+if(isset($_POST['username']) && isset($_POST['password'])){
+   $username = $_POST['username'];
+   $password = $_POST['password'];
+   $db = new SQLite3('db/database.db');
+   $results = $db->query("SELECT * FROM user WHERE username = '$username'");
+   $row = $results->fetcharray();
+   if($row[1]==$password){
+      header('Location: principale.php');
+   }
+   else
+   {
+      header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+   }
 }
 ?>
