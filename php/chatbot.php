0ed02b4a1d2d87e002ec13488b0576cbb02e7da7
GuillaumeGulli <guillaume.gulli@isen.yncrea.fr>
2021-06-01 11:22:24 +0200

    commit

---
+++ php/chatbot.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body class="neutral">
+    <!-- Chatbot -->
+    <div clas="chat_icon">
+        <i class="fa-solid fa-comments-question"></i>
+    </div>
+
+    <div class="chat_box">
+        
+    <!-- Chatbot -->
+    </body>
+</html>
+ 
