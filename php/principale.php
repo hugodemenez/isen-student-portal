261458477032499c122dc4f16702b5defb200234
Hugo Demenez <hdemenez@hotmail.fr>
2021-06-01 10:51:52 +0200

    commit

--- php/principale.php
+++ php/principale.php
@@ -11,7 +11,26 @@
             } else {
             echo "Vous n'êtes pas connecté";
             }
+            $apiKey = "ef7a9b79630b7257c06221b69e13fdb9";
+            $cityId = "2998324";
+            $googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=fr&units=metric&APPID=" . $apiKey;
+    
+            $ch = curl_init();
+    
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_VERBOSE, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($ch);
+    
+            curl_close($ch);
+            $meteo = json_decode($response);
+            echo "<br />".$meteo->weather[0]->description . "<br />";
+            echo $meteo->main->temp;
         ?>
+
     </body>
     <footer>
     <a href="login.php">Se déconnecter</a>
