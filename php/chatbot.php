02e7927735978c42b7e5e685979e6c56d700a573
GuillaumeGulli <guillaume.gulli@isen.yncrea.fr>
2021-06-02 16:45:40 +0200

    commit

--- php/chatbot.php
+++ php/chatbot.php
@@ -18,7 +18,22 @@
     <div class="chat_box">
         <div class="conv-form-wrapper">
         <form action="" method="GET" class="hidden">
-            
+            <input data-conv-question="Type in your e-mail" data-pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" id="email" type="email" name="email" required placeholder="What's your e-mail?">
+            <select name="programmer" data-conv-question="So, are you a programmer? (this question will fork the conversation based on your answer)">
+	            <option value="yes">Yes</option>
+	            <option value="no">No</option>
+            </select>
+            <div data-conv-fork="programmer">
+	            <div data-conv-case="yes">
+	 	            <input type="text" data-conv-question="A fellow programmer! Cool." data-no-answer="true">
+	            </div>
+	            <div data-conv-case="no">
+		            <select name="thought" data-conv-question="Have you ever thought about learning? Programming is fun!">
+			            <option value="yes">Yes</option>
+			            <option value="no">No..</option>
+		            </select>
+	            </div>
+            </div>
         </form>
         </div>
     </div>
