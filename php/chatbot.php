00c5b1acd0dada15a5d0952416bb8c8bfb61eec1
GuillaumeGulli <guillaume.gulli@isen.yncrea.fr>
2021-06-02 16:02:29 +0200

    commit

--- php/chatbot.php
+++ php/chatbot.php
@@ -1,11 +1,11 @@
 <html>
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="php\styles\jquery.convform.css">
     <script type="text/javascript" src="js\jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="js\custom.js"></script>
     <script type="text/javascript" src="js\jquery.convform.js"></script>
     <head>
     <meta charset="utf-8">
-        <link rel="stylesheet" href="chatbot_style.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="chatbot_style.css" media="screen" type="text/css">
     </head>
     <body class="neutral">
     <!-- Chatbot -->
@@ -14,9 +14,11 @@
     </div>
 
     <div class="chat_box">
-        <form>
+        <div class="conv-form-wrapper">
+        <form action="" method="GET" class="hidden">
             
         </form>
+        </div>
     </div>
     <!-- Chatbot -->
     </body>
