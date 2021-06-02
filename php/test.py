4246a60f4d25d901600828abcab9566068a700e4
Hugo Demenez <hdemenez@hotmail.fr>
2021-06-02 19:23:54 +0200

    commit

--- php/test.py
+++ php/test.py
@@ -1,5 +1,5 @@
 import sys,sqlite3,os
-from webscraping import scraping
+
 
 username = sys.argv[1]
 password = sys.argv[2]
@@ -7,25 +7,27 @@ email =sys.argv[3]
 niveau = sys.argv[4]
 specialite = sys.argv[5]
 
-
-print(username,password,email,niveau,specialite)
-path = "db/database.db"
-if os.path.isfile(path):
-    connexion = sqlite3.connect(path)
-    cursor = connexion.cursor()
-    #Add user to user table :
-    users=[{
-    'username' : username,
-    'password' : password,
-    'email' : email,
-    }]
-
-    for user in users:
-        parametres = (user['username'],user['password'],user['email'])
-        command = "INSERT OR REPLACE INTO user VALUES (?,?,?)"
-        cursor.execute(command,parametres)
-        connexion.commit()
-    cursor.execute("SELECT * FROM user")
+try:
     print("ok")
+    path = "../db/database.db"
+    if os.path.isfile(path):
+        connexion = sqlite3.connect(path)
+        cursor = connexion.cursor()
+        #Add user to user table :
+        users=[{
+        'username' : username,
+        'password' : password,
+        'email' : email,
+        }]
+
+        for user in users:
+            parametres = (user['username'],user['password'],user['email'])
+            command = "INSERT OR REPLACE INTO user VALUES (?,?,?)"
+            cursor.execute(command,parametres)
+            connexion.commit()
+        cursor.execute("SELECT * FROM user")
+except Exception as error:
+    print(error)
+
 
     
