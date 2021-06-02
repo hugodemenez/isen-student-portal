c791dc1201039e221c2ce2ea23ef0dceb8b49c01
Hugo Demenez <hdemenez@hotmail.fr>
2021-06-02 16:25:08 +0200

    commit

--- php/chatbot.php
+++ php/chatbot.php
@@ -1,12 +1,13 @@
 <html>
+    <link rel="stylesheet" href="styles\chatbot_style.css" media="screen" type="text/css">
     <link rel="stylesheet" type="text/css" href="styles\jquery.convform.css">
     <script type="text/javascript" src="js\jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="js\custom.js"></script>
     <script type="text/javascript" src="js\jquery.convform.js"></script>
     <script src="https://kit.fontawesome.com/ed342dc3ca.js" crossorigin="anonymous"></script>
     <head>
-    <meta charset="utf-8">
-        <link rel="stylesheet" href="styles\chatbot_style.css" media="screen" type="text/css">
+
+        
     </head>
     <body class="neutral">
     <!-- Chatbot -->
