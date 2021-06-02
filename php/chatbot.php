24102949f8d7e3299b2b2d918680404b3e5d27da
GuillaumeGulli <guillaume.gulli@isen.yncrea.fr>
2021-06-02 15:41:25 +0200

    commit

--- php/chatbot.php
+++ php/chatbot.php
@@ -1,4 +1,8 @@
 <html>
+    <link rel="stylesheet" type="text/css" href="">
+    <script type="text/javascript" src="js\jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="js\custom.js"></script>
+    <script type="text/javascript" src="js\jquery.convform.js"></script>
     <head>
     <meta charset="utf-8">
         <link rel="stylesheet" href="chatbot_style.css" media="screen" type="text/css" />
@@ -10,7 +14,10 @@
     </div>
 
     <div class="chat_box">
-        
+        <form>
+            
+        </form>
+    </div>
     <!-- Chatbot -->
     </body>
 </html>
---
+++ php/js/custom.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.chat_icon').click(function(event){
+        $('.chat_box').toggleClass('active')
+    })
+})
