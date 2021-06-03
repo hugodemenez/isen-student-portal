f84814a560b4a73db24d46e1acc73d576db8583d
Hugo Demenez <hdemenez@hotmail.fr>
2021-06-03 09:35:20 +0200

    using mysql

--- php/view/login_verification.php
+++ php/view/login_verification.php
@@ -3,10 +3,11 @@ ini_set('display_errors', 'on');
 if(isset($_POST['username']) && isset($_POST['password'])){
    $username = $_POST['username'];
    $password = $_POST['password'];
-   $db = new SQLite3('../db/database.db');
-   $results = $db->query("SELECT * FROM user WHERE username = '$username'");
-   $row = $results->fetcharray();
-   if($row[1]==$password){
+   include '../db/db_connection.php';
+   $conn = OpenCon();
+   $results = $conn->query("SELECT * FROM user WHERE username = '$username'");
+   $row = $results->fetch_assoc();
+   if($row["password"]==$password){
       session_start();
       $_SESSION['username'] = $username;
       $_SESSION['password'] = $password;
--- php/view/register_verification.php
+++ php/view/register_verification.php
@@ -6,8 +6,8 @@ $password_register=$_POST['password_register'];
 $email_register=$_POST['email_register'];
 $niveau_register=$_POST['niveau_register'];
 $specialite_register=$_POST['specialite_register'];
-
-$db = new SQLite3('../db/database.db');
-$results = $db->query("INSERT OR REPLACE INTO user VALUES ('$username_register','$password_register','$email_register')");
+include '../db/db_connection.php';
+$conn = OpenCon();
+$results = $conn->query("INSERT OR REPLACE INTO user VALUES ('$username_register','$password_register','$email_register')");
 header('Location: ../index.php?register_error=2');
 ?>
