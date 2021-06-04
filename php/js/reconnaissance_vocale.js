3b557514cc0c5478de8fb86f570ca0735f40c896
Brieuc DANDOY <brieuc.dandoy@isen.yncrea.fr>
2021-06-04 15:01:53 +0200

    asas

--- php/js/reconnaissance_vocale.js
+++ php/js/reconnaissance_vocale.js
@@ -29,7 +29,7 @@ recognition.onresult = function(event)
 function synthetiser(message){
 	let msg = new SpeechSynthesisUtterance();
 	let voices = window.speechSynthesis.getVoices();
-	msg.voice = voices[10]; // Note: some voices don't support altering params
+	msg.voice = voices[6]; // Note: some voices don't support altering params
 	msg.voiceURI = 'native';
 	msg.volume = 1; // 0 to 1
 	msg.rate = 1; // 0.1 to 2
