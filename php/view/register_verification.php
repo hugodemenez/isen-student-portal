b466586fcaccf7cc3cdbc8f9f7ec8b99697bb7ca
Hugo Demenez <hdemenez@hotmail.fr>
2021-06-14 13:42:04 +0200

    commit

--- php/view/register_verification.php
+++ php/view/register_verification.php
@@ -15,12 +15,12 @@ else{
     $item=$_POST['username_register'];
     $item2=$_POST['password_register'];
     set_time_limit (20);
-    $tmp = exec("python3 /home/ubuntu/aurion_check.py $item $item2");
+    $tmp = exec("python3 ../scripts/aurion_check.py $item $item2");
     echo $tmp;
     if ($tmp==true){
         $results = $conn->query("INSERT INTO user VALUES ('$username_register','$password_register','$email_register')");
         $_SESSION['username']=$username_register;
-        file_put_contents("/home/ubuntu/waiting_list.txt", "1");
+        file_put_contents("../scripts/waiting_list.txt", "1");
         header('Location: ../principale.php');
     }
     
