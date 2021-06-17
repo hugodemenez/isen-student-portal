e31f3899784ccf4ef0da01ee284a16f5ddec5c0f
Brieuc DANDOY <brieuc.dandoy@isen.yncrea.fr>
2021-06-17 14:27:36 +0200

    comit

--- php/js/reconnaissance_vocale.js
+++ php/js/reconnaissance_vocale.js
@@ -67,7 +67,9 @@ function comprendre(texte){ //regex pour comprendre la commande par exemple si l
 	}
 	else if (texte.search(/note/) != -1) {
 		alert(caractere_cpeciaux(readCookie('Cookie_note')));
-		message_synthetise = "voici votre dernière note :" + caractere_cpeciaux(readCookie("Cookie_note")).replace(/_/g," ");
+		var txtp = caractere_cpeciaux(readCookie("Cookie_note"));
+		txtp = txt.replace("_"," ");
+		message_synthetise = "voici votre dernière note :" + txtp;
 		return "note";
 	}
 	else {texte_comprendre = "-1";}
