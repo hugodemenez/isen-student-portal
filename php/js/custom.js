11ca3ca254f86526967d75a001145104dffe657a
Hugo Demenez <hdemenez@hotmail.fr>
2021-06-25 13:48:30 +0200

    commit

--- php/js/custom.js
+++ php/js/custom.js
@@ -1,3 +1,5 @@
+
+// Definition de la fonction pour activer le chatbot lors du clique
 $(document).ready(function(){
     $('.chat_icon').click(function(event){
         $('.chat_box').toggleClass('active')
